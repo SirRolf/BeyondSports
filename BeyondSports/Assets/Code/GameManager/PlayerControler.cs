3760336ab036b19833199fc882e44ae9fdb3bbde
Floris <Flomo123456789@gmail.com>
2019-12-19 09:18:54 +0100

    fixed a performance isue

--- BeyondSports/Assets/Code/GameManager/PlayerControler.cs
+++ BeyondSports/Assets/Code/GameManager/PlayerControler.cs
@@ -6,12 +6,13 @@ using System.IO;
 public class PlayerControler : MonoBehaviour
 {
     static string path = "Assets/Data/match_data.dat";
-
-    private float frameCount = 0;
+    
     private float timer = 0;
 
     [SerializeField]
     private GameObject player;
+
+    private StreamReader reader = new StreamReader(path);
 
     void Start()
     {
@@ -29,7 +30,7 @@ public class PlayerControler : MonoBehaviour
         if (timer <= 4)
         {
             timer += Time.deltaTime;
-            string[] tokens = ReadString(frameCount, 1).Split(';');
+            string[] tokens = ReadString(1).Split(';');
             for (int i = 0; i < tokens.Length - 1; i++)
             {
                 string[] individualPlayerData = tokens[i].Split(',');
@@ -38,8 +39,6 @@ public class PlayerControler : MonoBehaviour
                 myPlayer.transform.GetChild(0).GetComponent<PlayerNumber>().ChangeTeam(individualPlayerData[2]);
                 myPlayer.GetComponent<PlayerMovement>().Move(float.Parse(individualPlayerData[3]), float.Parse(individualPlayerData[4]));
             }
-
-            frameCount++;
         }
         else
         {
@@ -47,15 +46,9 @@ public class PlayerControler : MonoBehaviour
         }
     }
 
-    private string ReadString(float line, int dataObject)
+    private string ReadString(int dataObject)
     {
-        StreamReader reader = new StreamReader(path);
-        for (int i = 0; i < line; i++)
-        {
-            reader.ReadLine();
-        }
         string[] tokens = reader.ReadLine().Split(':');
-        reader.Close();
         return tokens[dataObject];
     }
 }
